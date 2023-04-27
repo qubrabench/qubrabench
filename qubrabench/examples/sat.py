e95a8696c9a08ebfbb487d0533ca6a4f5e9a06c4
anurudhp <anurudhp97@gmail.com>
2023-04-27 18:41:10 +0200

    add simple SAT example: run benchmark on a fixed small SAT instance.

---
+++ qubrabench/examples/sat.py
@@ -0,0 +1,86 @@
+from dataclasses import asdict
+import numpy as np
+import random
+import logging
+import pandas as pd
+
+from qubrabench.bench.stats import QueryStats
+from qubrabench.algorithms.search import search
+
+from dataclasses import dataclass
+import numpy as np
+import scipy
+
+
+@dataclass(frozen=True)
+class SatInstance:
+    """
+    As in 4.3.2 in Cade et al, clauses are represented by vectors in {-1,0,1}^n and assignments by vectors in {-1,1}^n.
+    """
+
+    k: int  # number of literals per clause
+    clauses: np.ndarray  # (clauses_no x literals_no) matrix
+
+    @property
+    def n(self):
+        """Number of variables"""
+        return self.clauses.shape[1]
+
+    @property
+    def m(self):
+        """Number of clauses"""
+        return self.clauses.shape[0]
+
+    def evaluate(self, assignment):
+        clauses = (self.clauses * assignment) > 0
+        return np.all([np.any(c) for c in list(clauses)])
+
+    @staticmethod
+    def random(k, n, m, *, seed=None, random_weights=None):
+        """
+        Generate a random k-SAT instance with n variables and m clauses.
+        """
+        if random_weights is None:
+            random_weights = np.random.random
+
+        # generate random clauses (m x n matrix)
+        if seed is not None:
+            np.random.seed(seed)
+        clauses = np.zeros(shape=(m, n), dtype=int)
+        for i in range(m):
+            vs = np.random.choice(n, k, replace=False)
+            clauses[i][vs] = np.random.choice([-1, 1], k)
+
+        clauses = scipy.sparse.csr_matrix(clauses)
+
+        return SatInstance(k, clauses)
+
+
+def run_specific_instance():
+    inst = SatInstance(
+        k=2, clauses=np.array([[1, 0, 0], [0, 1, 1], [0, -1, -1]], dtype=int)
+    )
+
+    # TODO: run multiple times to obtain proper statistics 
+    n = inst.n
+    search_space = np.full((2**n, n), 1, dtype=int)
+    for i in range(n):
+        for start in range(0, 2**n, 2 ** (i + 1)):
+            for j in range(2**i):
+                search_space[start + j, i] = -1
+
+    stats = QueryStats()
+    search(search_space, lambda x: inst.evaluate(x), stats=stats, eps=10**-5)
+
+    stats = asdict(stats)
+    stats["n"] = n
+    stats["k"] = inst.k
+    stats["m"] = inst.m
+
+    T = 0
+    for x in list(search_space):
+        if inst.evaluate(x):
+            T += 1
+    stats["T"] = T
+
+    return stats
---
+++ tests/test_sat.py
@@ -0,0 +1,35 @@
+import time
+import pytest
+from pytest_check import check
+
+import numpy as np
+from qubrabench.examples.sat import run_specific_instance as run
+
+def equals(x):
+    return lambda y: x == y
+def isclose(x):
+    return lambda y: np.isclose(x, y)
+
+
+@pytest.mark.rub
+def test_simple_sat():
+    n = 3
+    k = 2
+    m = 3
+
+    stats = run()
+
+    def verify(stat, checker):
+        with check:
+            assert checker(stats[stat]), f"Stat `{stat}` does not match! got: {stats[stat]}"
+
+    verify("n", equals(n))
+    verify("k", equals(k))
+    verify("m", equals(m))
+
+    verify("T", equals(2))
+
+    verify("classical_actual_queries", equals(2))
+    verify("classical_expected_queries", equals(3))
+    verify("quantum_expected_classical_queries", isclose(4))
+    verify("quantum_expected_quantum_queries", isclose(0))
