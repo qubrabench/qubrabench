510243b0f492b4eb05771c26cc1e13fc0c6c3095
Michael Walter <michael.walter@gmail.com>
2023-04-28 10:02:18 +0200

    hillclimber_kit: Don't shuffle neighbors (search already shuffles)

--- examples/sat/hillclimber_kit.py
+++ examples/sat/hillclimber_kit.py
@@ -164,7 +164,7 @@ def get_neighbours(solution, max_hamming_distance):
 
     neighbours = list(neighbours)
     # random.seed(current_seed)
-    random.shuffle(neighbours)
+    # random.shuffle(neighbours)
     return neighbours
 
 
