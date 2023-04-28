bc7b82646feea8e9c5722da0c26914f7564f7773
Michael Walter <michael.walter@gmail.com>
2023-04-28 09:23:38 +0200

    One change to max so that it behaves like the built-in max

--- qubrabench/algorithms/max.py
+++ qubrabench/algorithms/max.py
@@ -1,17 +1,28 @@
+from typing import Iterable, TypeVar, Optional
 from qubrabench.bench.stats import QueryStats
 
+T = TypeVar("T")
 
-def max(iterable, default=None, key=None, *, eps=10**-5, stats: QueryStats = None):
+
+def max(
+    iterable: Iterable[T],
+    *,
+    default: Optional[T] = None,
+    key=None,
+    eps=10**-5,
+    stats: QueryStats = None,
+):
     iterator = iter(iterable)
     try:
         max_val = next(iterator)
     except StopIteration:
+        if default is None:
+            raise ValueError("max() arg is an empty sequence")
         return default
     if key is None:
         key = lambda x: x
     for elem in iterator:
         stats.classical_actual_queries += 1
-        # This condition corresponds to the function f_i(j) as seen in the paper Cade et al. eq (7)
         if key(elem) > key(max_val):
             max_val = elem
     return max_val
