85b01ca9aaef0c70202d4f9a4fe1df22121cfc86
anurudhp <anurudhp97@gmail.com>
2023-05-01 23:02:49 +0200

    [tests] cleanup max test

--- tests/algorithms/test_max.py
+++ tests/algorithms/test_max.py
@@ -5,24 +5,9 @@ from qubrabench.algorithms.max import max
 from qubrabench.bench.stats import QueryStats
 
 
-def test_max():
-    list_to_search = range(0, 100)
-    stats = QueryStats()
-
-    # with check:
-    #     assert (
-    #         search(list_to_search, lambda it: it == 50, eps=10**-5, stats=stats) == 50
-    #     )
-    # with check:
-    #     assert stats.classical_control_method_calls == 0
-    # with check:
-    #     assert stats.classical_actual_queries == 4
-    # with check:
-    #     assert stats.classical_expected_queries == 50.5
-    # with check:
-    #     assert stats.quantum_expected_classical_queries == 72.9245740488006
-    # with check:
-    #     assert stats.quantum_expected_quantum_queries == 18.991528740664712
+def test_max_return_value():
+    N = 100
+    assert max(range(N), eps=10**-5) == N - 1
 
 
 def test_max_raises_on_empty_and_no_default():
