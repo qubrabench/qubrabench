0dc17e00a2c6e2c57604510642a6018a04ce72f0
Tim Bittner <timbittner21@gmail.com>
2023-05-05 09:51:26 +0200

    [examples][cd] add classical Louvain algorithm
    also added unit tests
    modularity does not behave as expected to me, see in code/test TODOs
    #17

---
+++ examples/communitydetection/louvain.py
@@ -0,0 +1,178 @@
+from networkx.generators.community import LFR_benchmark_graph
+import networkx as nx
+
+
+def random_lfr_graph(
+    n: int,
+    *,
+    tau1: float = 3,
+    tau2: float = 2,
+    mu: float = 0.3,
+    max_community: float = 100,
+    max_degree: int = 100,
+    average_degree: float = 5,
+    seed=None,
+):
+    """
+    Generates an LFR benchmark graph instance with defaults similar to Cade et al.
+    Sacrifices exact node number for ensuring no selfloops
+    """
+    graph = LFR_benchmark_graph(
+        n,
+        tau1=tau1,
+        tau2=tau2,
+        mu=mu,
+        max_community=max_community,
+        max_degree=max_degree,
+        average_degree=average_degree,
+        seed=seed,
+    )
+    graph.remove_edges_from(nx.selfloop_edges(graph))
+    graph.remove_nodes_from(list(nx.isolates(graph)))
+    graph = nx.relabel.convert_node_labels_to_integers(graph)
+    return graph
+
+
+class Louvain:
+    """
+    This class implements the classical Louvain algorithm like Cade et al. do in their community detection paper.
+    It is initialized using an undirected networkx graph instance without selfloops.
+    """
+
+    def __init__(self, G) -> None:
+        """Initialize Louvain algorithm based on a graph instance
+
+        Args:
+            G (nx.Graph): The initial graph where communities should be detected.
+        """
+        self.update_graph(G)
+        self.C = self.single_partition()
+
+    def update_graph(self, new_graph):
+        # TODO fix conversion later for history?
+        self.G = nx.relabel.convert_node_labels_to_integers(new_graph)
+        self.A = nx.adjacency_matrix(new_graph)
+        self.W = self.A.sum() / 2
+
+    def louvain(self):
+        """
+        Start the Louvain heuristic algorithm for community detection.
+        """
+        done = False
+        while not done and self.G.order() > 1:
+            self.move_nodes()  # updates the community labeling function
+            done = self.G.order() == len(set(self.C.values()))
+            if not done:
+                print("Aggregating Graph")
+                self.aggregate_graph()
+                self.C = self.single_partition()
+        return self.C
+
+    def move_nodes(self):
+        """Reassign nodes to new communities based on change in modularities."""
+        done = False
+        while not done:
+            done = True
+            for u in self.G:
+                # calculate maximum increase of modularity
+                # TODO if we have differing values check these parameters
+                max_modularity_increase, v_community = max(
+                    [
+                        (self.delta_modularity(u, self.C[v]), self.C[v])
+                        for v in self.G[u]
+                    ],
+                    key=lambda entry: entry[0],
+                )
+                # TODO: is there something wrong with the delta_modularity function or do we need an epsilon
+                if max_modularity_increase > 0:
+                    # reassign u to community of v
+                    print(
+                        f"Reassigned u{u} from C{self.C[u]} to C{v_community} with delta:{max_modularity_increase}"
+                    )
+                    self.C[u] = v_community
+                    done = False
+                # terminate when there is no modularity increase
+
+    def aggregate_graph(self):
+        """
+        Create a new, coarser graph where every node is a community of the current graph instance.
+        1. Create a new vertex for every non-empty set
+        2. Create edges with weight equal to the sum of all weights between vertices in each community
+        """
+        G_new = nx.Graph()
+        for i, community in enumerate(set(self.C.values())):
+            G_new.add_node(community)
+
+        for u, v, weight in self.G.edges(data="weight", default=1):
+            # we only care about edges between communities
+            if self.C[u] != self.C[v]:
+                w = self.C[u]
+                x = self.C[v]
+
+                if not G_new.has_edge(w, x):
+                    # create edge
+                    G_new.add_edge(w, x, weight=weight)
+                # update weight
+                G_new.get_edge_data(w, x)["weight"] += weight
+
+        print(list(nx.isolates(G_new)))
+        print(self.C)
+        self.update_graph(G_new)
+
+    def single_partition(self):
+        """Assign every node to its own unique community"""
+        community_set = {}
+        for node in self.G:
+            community_set[node] = node
+        return community_set
+
+    def delta_modularity(self, u: int, alpha: int) -> float:
+        """Calculate the change in modularity that would occur if u was moved into community alpha
+
+        Args:
+            u (int): Integer label of the nx.Node to be moved
+            alpha (int): Integer label of the target community
+
+        Returns:
+            float: The resulting change in modularity
+        """
+        # TODO in Cade the modularity equations (3) and (4) differ by a ± sign, which one to choose?
+        return ((self.S(u, alpha) - self.S(u, self.C[u])) / self.W) - (
+            self.strength(u)
+            * (self.Sigma(alpha) - self.Sigma(self.C[u]) + self.strength(u))
+        ) / (2 * (self.W**2))
+
+    def S(self, u: int, alpha: int) -> float:
+        """Strength of u to other nodes in the community alpha
+
+        Args:
+            u (int): Integer label of the nx.Node whose strength should be determined
+            alpha (int): Integer label of the community that the incident edges of u belong to
+
+        Returns:
+            float: The resulting strength
+        """
+        return sum(
+            [self.A[u, v] for v, community in self.C.items() if community == alpha]
+        )
+
+    def Sigma(self, alpha: int) -> float:
+        """Calculates sum of all weights on edges incident to vertices contained in a neighboring community
+
+        Args:
+            alpha (int): Integer label of the community whose Sigma is to be calculated
+
+        Returns:
+            float: Sum of weights
+        """
+        sigma = 0
+        for v, community in self.C.items():
+            if community == alpha:
+                for x, y, weight in self.G.edges(v, data="weight", default=1):
+                    if self.C[x] != self.C[y]:
+                        sigma += weight
+
+        return sigma
+
+    def strength(self, u: int) -> float:
+        return self.G.degree(u, weight="weight")
---
+++ examples/communitydetection/test_louvain.py
@@ -0,0 +1,111 @@
+from pytest_check import check
+import numpy as np
+import networkx as nx
+
+import louvain
+
+
+small_graph_example = np.array(
+    [  # A, A, A, A, A, B, B, B, B, B
+        [0, 9, 0, 1, 2, 1, 7, 4, 0, 0],  # relations of node 0
+        [9, 0, 2, 1, 0, 6, 9, 3, 8, 6],
+        [0, 2, 0, 1, 1, 2, 4, 8, 4, 9],
+        [1, 1, 1, 0, 0, 3, 8, 0, 4, 4],
+        [2, 0, 1, 0, 0, 6, 8, 5, 3, 4],
+        [1, 6, 2, 3, 6, 0, 9, 4, 4, 8],
+        [7, 9, 4, 8, 8, 9, 0, 8, 8, 9],
+        [4, 3, 8, 0, 5, 4, 8, 0, 7, 5],
+        [0, 8, 4, 4, 3, 4, 8, 7, 0, 0],
+        [0, 6, 9, 4, 4, 8, 9, 5, 0, 0],
+    ]
+)
+
+
+def sanity_check_input(A):
+    """
+    checks that a given adjacencry matrix has 0 for all diagonal elements, as required by Cade et al. (Community Detection)
+    """
+    check.equal(sum(A.diagonal()), 0)
+
+
+def test_node_to_community_strength():
+    """
+    Test the various calculations used in Louvain algorithms
+    """
+    # generate graph instance
+    G = nx.from_numpy_array(small_graph_example)
+    solver = louvain.Louvain(G)
+
+    # setup community
+    split_index = int(G.order() / 2)  # split initial communities into halves
+    community_alpha = 0
+    community_beta = 1
+    C = {}
+    for n in G:
+        C[n] = community_alpha if n < split_index else community_beta
+    solver.C = C
+
+    # compute strength of node and neighbors in community alpha
+    target_node = 0
+    com_node_str = 0
+    for value in small_graph_example[0][:split_index]:
+        com_node_str += value
+    check.equal(solver.S(target_node, community_alpha), com_node_str)
+
+    # compute strength of all nodes in community alpha
+    com_str = 0
+    for row in small_graph_example[:split_index]:
+        for value in row[split_index:]:
+            com_str += value
+    check.equal(solver.Sigma(community_alpha), com_str)
+
+    # compute W
+    W = 0
+    for row in small_graph_example:
+        for value in row:
+            W += value
+    W /= 2
+    check.equal(solver.W, W)
+
+    # validate strength
+    check.equal(solver.strength(target_node), sum(small_graph_example[target_node]))
+
+    # determine delta modularity for moving node 0 to beta
+    # TODO Why do these checks both fail, I would assume that a potential move to its own community should not change modularity
+    # check.equal(solver.delta_modularity(target_node, community_alpha), 0)
+    # check.equal(solver.delta_modularity(target_node, community_beta), 0)
+
+
+def test_move_nodes():
+    """
+    TODO test the reassignment of communities without aggregating the graph
+    """
+    # setup rng
+    rng = np.random.default_rng(seed=123)
+
+    # generate graph instance
+    G = louvain.random_lfr_graph(1000, seed=rng)
+    solver = louvain.Louvain(G)
+    sanity_check_input(solver.A)
+
+    # determine delta modularity for moving node 0 to community 1
+    # TODO Why do these checks both fail, I would assume that a potential move to its own community should not change modularity
+    # check.equal(solver.delta_modularity(0, 1), 0)
+    # check.equal(solver.delta_modularity(0, 0), 0)
+
+
+def test_louvain():
+    """
+    TODO test an entire louvain pass
+    """
+    # setup rng
+    rng = np.random.default_rng(seed=123)
+
+    # generate graph instance
+    G = louvain.random_lfr_graph(100, seed=rng)
+    solver = louvain.Louvain(G)
+    sanity_check_input(solver.A)
+
+    print(solver.G)
+    solver.louvain()
+    print(solver.G)
