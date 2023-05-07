e831c6f94bb6e7c5bcad01c3abb39d556503f4a2
Michael Walter <michael.walter@gmail.com>
2023-05-07 20:45:29 +0200

    [examples.sat.bruteforce] refactor so that it matches hillclimber in terms of design

--- examples/sat/bruteforce.py
+++ examples/sat/bruteforce.py
@@ -1,47 +1,21 @@
-from dataclasses import asdict
 from typing import Optional
+import itertools
 import numpy as np
-import pandas as pd
-
-from sat import SatInstance
-from qubrabench.bench.stats import QueryStats
+from sat import SatInstance, Assignment
+from qubrabench.stats import QueryStats
 from qubrabench.algorithms.search import search
 
 
-def run_specific_instance(
+# TODO: should not return QueryStats
+def bruteforce_solve(
     inst: SatInstance,
     *,
     rng: np.random.Generator,
-    n_runs: int,
     eps: Optional[float] = None,
-):
-    n = inst.n
-    search_space = np.full((2**n, n), 1, dtype=int)
-    for i in range(n):
-        for start in range(0, 2**n, 2 ** (i + 1)):
-            for j in range(2**i):
-                search_space[start + j, i] = -1
+    stats: Optional[QueryStats] = None,
+) -> Optional[Assignment]:
+    # list of all bitstrings
+    domain = [np.array(x, dtype=int) for x in itertools.product([-1, 1], repeat=inst.n)]
 
-    T = 0
-    for x in list(search_space):
-        if inst.evaluate(x):
-            T += 1
-
-    history = []
-    for run_ix in range(n_runs):
-        stats = QueryStats()
-        search(search_space, inst.evaluate, eps=eps, stats=stats, rng=rng)
-
-        stats = asdict(stats)
-        stats["n"] = inst.n
-        stats["k"] = inst.k
-        stats["m"] = inst.m
-        stats["T"] = T
-        history.append(stats)
-
-    history = pd.DataFrame(
-        [list(row.values()) for row in history],
-        columns=stats.keys(),
-    )
-
-    return history
+    # brute-force search
+    return search(domain, inst.evaluate, eps=eps, stats=stats, rng=rng)
--- examples/sat/test_bruteforce.py
+++ examples/sat/test_bruteforce.py
@@ -1,28 +1,28 @@
 import numpy as np
-
 from pytest_check import check
 
+from qubrabench.stats import QueryStats
 from sat import SatInstance
-from bruteforce import run_specific_instance
+from bruteforce import bruteforce_solve
 
 
-def test_simple_sat_instance():
+def test_solve() -> None:
+    # solve a simple SAT instance
     inst = SatInstance(
-        k=2, clauses=np.array([[1, 0, 0], [0, 1, 1], [0, -1, -1]], dtype=int)
+        k=2,
+        clauses=np.array([[1, 1, 0], [1, -1, 0], [0, 1, 1], [0, -1, -1]], dtype=int),
+    )
+    stats = QueryStats()
+    x = bruteforce_solve(
+        inst, rng=np.random.default_rng(seed=12), eps=10**-5, stats=stats
     )
 
-    history = run_specific_instance(
-        inst, n_runs=200, eps=10**-5, rng=np.random.default_rng(seed=12)
-    )
-    history = history.groupby(["n", "k", "m"]).mean(numeric_only=True).reset_index()
+    # check that found a solution
+    assert inst.evaluate(x)
 
-    check.equal(history.loc[0, "n"], 3)
-    check.equal(history.loc[0, "k"], 2)
-    check.equal(history.loc[0, "m"], 3)
-    check.equal(history.loc[0, "T"], 2)
-
-    check.almost_equal(history.loc[0, "classical_control_method_calls"], 0)
-    check.almost_equal(history.loc[0, "classical_actual_queries"], 3.285)
-    check.almost_equal(history.loc[0, "classical_expected_queries"], 3)
-    check.almost_equal(history.loc[0, "quantum_expected_classical_queries"], 4)
-    check.almost_equal(history.loc[0, "quantum_expected_quantum_queries"], 0)
+    # check stats
+    check.equal(stats.classical_control_method_calls, 0)
+    check.equal(stats.classical_actual_queries, 2)
+    check.almost_equal(stats.classical_expected_queries, 3)
+    check.almost_equal(stats.quantum_expected_classical_queries, 4)
+    check.almost_equal(stats.quantum_expected_quantum_queries, 0)
