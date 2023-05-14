15903e61abe5e3bc2f18f320bda834239ca208b1
Luca-sudo <luca.witt@rub.de>
2023-05-14 10:59:04 +0200

    [examples.sat.schoning] Added Test with values currently generated.

---
+++ examples/sat/test_schoning.py
@@ -0,0 +1,27 @@
+import numpy as np
+from pytest_check import check
+
+from qubrabench.stats import QueryStats
+from sat import SatInstance
+from schoning import schoning_solve
+
+
+def test_solve(rng) -> None:
+    # solve a simple SAT instance
+    inst = SatInstance(
+        k=2,
+        clauses=np.array([[1, 1, 0], [1, -1, 0], [0, 1, 1], [0, -1, -1]], dtype=int),
+    )
+    stats = QueryStats()
+    x = schoning_solve(inst, rng=rng, eps=10**-5, stats=stats)
+    assert x is not None
+
+    # check that found a solution
+    assert inst.evaluate(x)
+
+    # check stats
+    check.equal(stats.classical_control_method_calls, 0)
+    check.equal(stats.classical_actual_queries, 1)
+    check.almost_equal(stats.classical_expected_queries, 2.5)
+    check.almost_equal(stats.quantum_expected_classical_queries, 3)
+    check.almost_equal(stats.quantum_expected_quantum_queries, 0)
