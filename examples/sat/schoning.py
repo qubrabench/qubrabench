41c76e487814453257b03a93f58d51bc1ce85542
Luca-sudo <witt-luca@web.de>
2023-05-15 13:33:57 +0200

    [examples.sat.schoning] Improved formatting.

--- examples/sat/schoning.py
+++ examples/sat/schoning.py
@@ -13,14 +13,10 @@ def schoning_solve(
     eps: Optional[float] = None,
     stats: Optional[QueryStats] = None,
 ) -> Optional[Assignment]:
-
     domain = [np.array(x, dtype=int) for x in itertools.product([-1, 1], repeat=inst.n)]
 
     # Predicate for the search function. Uses SatInstance and rng from context.
-    def schoning(
-        x: np.array
-    ) -> bool:
-
+    def schoning(x: np.array) -> bool:
         n = inst.n
         for _ in range(0, 3 * n):
             if inst.evaluate(x):
@@ -31,8 +27,7 @@ def schoning_solve(
 
     return search(domain, schoning, eps=eps, stats=stats, rng=rng)
 
-def flip_random_variable(
-    x: np.ndarray, rng: np.random.Generator
-) -> np.ndarray:
+
+def flip_random_variable(x: np.ndarray, rng: np.random.Generator) -> np.ndarray:
     x[rng.choice(np.arange(len(x)))] *= -1
     return x
