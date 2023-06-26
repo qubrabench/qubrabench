bad73f97688c744d04a34ef7cad032fc65e8536f
Tim Bittner <timbittner21@gmail.com>
2023-06-26 15:10:53 +0200

    [broken][cd] nested search needs values from predicate

--- examples/communities/qlouvain.py
+++ examples/communities/qlouvain.py
@@ -9,13 +9,19 @@ from louvain import Louvain
 
 from qubrabench.algorithms.search import search as qsearch
 from qubrabench.algorithms.max import max as qmax
+from qubrabench.stats import QueryStats
 
 
 class QLouvain(Louvain):
     """Direct quantum version with the same behavior as classical Louvain"""
 
     def __init__(
-        self, G, *, rng: np.random.Generator, keep_history: bool = False
+        self,
+        G,
+        *,
+        rng: np.random.Generator,
+        keep_history: bool = False,
+        error: float = 10e-5,
     ) -> None:
         """Initialize Louvain algorithm based on a graph instance
 
@@ -23,19 +29,22 @@ class QLouvain(Louvain):
             G (nx.Graph): The initial graph where communities should be detected.
             keep_history: Keep maintaining a list of adjacency matrices and community mappings. Defaults to False.
             rng: Source of randomness
+            error: upper bound on the failure probability of the quantum algorithm.
         """
         Louvain.__init__(self, G, keep_history=keep_history)
         self.rng = rng
+        self.stats = QueryStats()
+        self.error = error
 
     def pred(self, node: int) -> bool:
         """A predicate which itself runs a grover search over neighboring nodes"""
         result = qmax(
             self.G[node],
-            key=lambda neighbor: bool(
-                self.delta_modularity(node, self.C[neighbor]) > 0
-            ),
+            key=lambda neighbor: self.delta_modularity(node, self.C[neighbor]),
+            stats=self.stats,
+            error=self.error,
         )
-        return result is not None
+        return self.delta_modularity(node, self.C[result]) > 0
 
     def move_nodes(self):
         done = False
@@ -45,10 +54,15 @@ class QLouvain(Louvain):
                 self.G.nodes,
                 self.pred,
                 rng=self.rng,
+                stats=self.stats,
+                error=self.error,
             )
 
             if result:
-                node, neighbor = result
+                (
+                    node,
+                    neighbor,
+                ) = result  # Result only contains one node, information about the neighbor is lost in predicate
                 # reassign node to community of neighbor
                 self.C[node] = self.C[neighbor]
                 done = False
@@ -77,7 +91,8 @@ class SimpleQLouvain(QLouvain):
             self.G[node],
             lambda neighbor: bool(self.delta_modularity(node, self.C[neighbor]) > 0),
             rng=self.rng,
-            # TODO add stats object
+            stats=self.stats,
+            error=self.error,
         )
         return result is not None
 
@@ -105,12 +120,14 @@ class EdgeQLouvain(Louvain):
             done = True
 
             # calculate maximum increase of modularity
-            node, max_modularity_increase, v_community = max(
+            node, max_modularity_increase, v_community = qmax(
                 [
                     (u, self.delta_modularity(u, self.C[v], exact=True), self.C[v])
                     for u, v in self.G.edges
                 ],
                 key=lambda entry: entry[0],
+                stats=self.stats,
+                error=self.error,
             )
             if max_modularity_increase > 0:
                 # reassign u to community of v
