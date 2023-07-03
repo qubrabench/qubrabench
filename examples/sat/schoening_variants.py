ea0219b1a0d476c87b178ebb67c99b26d0c73ee5
Luca <witt-luca@web.de>
2023-07-03 11:57:17 +0200

    [examples][schoening] Added schoening variant with mixed bruteforce.

--- examples/sat/schoening_variants.py
+++ examples/sat/schoening_variants.py
@@ -7,7 +7,11 @@ from qubrabench.stats import QueryStats
 from qubrabench.algorithms.search import search
 from schoening import schoening_with_randomness
 
-__all__ = ["schoening_bruteforce_assignment"]
+__all__ = [
+    "schoening_bruteforce_assignment",
+    "schoening_bruteforce_steps",
+    "schoening_mixed_solve",
+]
 
 
 def schoening_bruteforce_assignment(
@@ -106,5 +110,90 @@ def schoening_bruteforce_steps(
 
     # return satisfying assignment (if any was found)
     if sat_steps is not None:
+        return schoening_with_randomness(sat_pair, inst)
+    return None
+
+
+def schoening_mixed_solve(
+    inst: SatInstance,
+    *,
+    rng: np.random.Generator,
+    error: Optional[float] = None,
+    stats: Optional[QueryStats] = None,
+) -> Optional[Assignment]:
+    """
+    Find a satisfying assignment of a 3-SAT formula by using a variant of Schöning's algorithm,
+    which bruteforces over a subset of both assignments and steps.
+
+    Args:
+        inst: The 3-SAT Instance for which to find a satisfying assignment.
+        rng: Random number generator.
+        error: Allowed failure probability.
+        stats: Object that keeps track of statistics about evaluation queries to the SAT instance.
+
+    Returns:
+        Satisfying assignment if found, None otherwise.
+    """
+    assert inst.k == 3
+
+    # FIXME: this defines the length of both the randomness and assignment to bruteforce over. What value should this have?
+    bruteforce_length = 2
+
+    # prepare search domain comprising partial assignments and partial steps
+    n = inst.n
+    assignments = [
+        np.array(x, dtype=int)
+        for x in itertools.product([-1, 1], repeat=n - bruteforce_length)
+    ]
+    steps = [
+        np.array(s, dtype=int)
+        for s in itertools.product([0, 1, 2], repeat=3 * (n - bruteforce_length))
+    ]
+    domain = itertools.product(assignments, steps)
+
+    # the space over which to bruteforce
+    bruteforce_assignments = [
+        np.array(x, dtype=int)
+        for x in itertools.product([-1, 1], repeat=bruteforce_length)
+    ]
+    bruteforce_steps = [
+        np.array(s, dtype=int)
+        for s in itertools.product([0, 1, 2], repeat=3 * bruteforce_length)
+    ]
+    bruteforce = itertools.product(bruteforce_assignments, bruteforce_steps)
+
+    # variable for the oracle to store string that yields sat assignment
+    sat_string = None
+
+    # find a choice of randomness that makes Schoening's algorithm accept
+    def pred(x):
+        # split initial input up into assignment and steps
+        initial_assignment, initial_steps = x
+        for string in bruteforce:
+            # join initial and bruteforce parts for search
+            assignment, steps = string
+            item = (
+                np.concatenate((initial_assignment, assignment), dtype=None),
+                np.concatenate((initial_steps, steps), dtype=None),
+            )
+
+            # if current assignment is satisfying, sets variable in outer scope
+            if schoening_with_randomness(item, inst) is not None:
+                nonlocal sat_string
+                sat_string = string
+                return True
+        return False
+
+    # join result of search with the result of bruteforce stored by oracle
+    randomness = search(domain, pred, error=error, stats=stats, rng=rng)
+    initial_assignment, initial_steps = randomness
+    assignment, steps = sat_string
+    sat_pair = (
+        np.concatenate((initial_assignment, assignment), dtype=None),
+        np.concatenate((initial_steps, steps), dtype=None),
+    )
+
+    # return satisfying assignment (if any was found)
+    if sat_pair is not None:
         return schoening_with_randomness(sat_pair, inst)
     return None
--- examples/sat/test_schoening_variants.py
+++ examples/sat/test_schoening_variants.py
@@ -7,6 +7,7 @@ from sat import SatInstance
 from schoening_variants import (
     schoening_bruteforce_assignment,
     schoening_bruteforce_steps,
+    schoening_mixed_solve,
 )
 
 
@@ -51,6 +52,34 @@ def test_bruteforce_steps(rng) -> None:
     )
     stats = QueryStats()
     x = schoening_bruteforce_steps(inst, rng=rng, error=10**-5, stats=stats)
+    assert x is not None
+
+    # check that found a solution
+    assert inst.evaluate(x)
+
+    # check stats
+    assert stats == QueryStats(
+        classical_control_method_calls=0,
+        classical_actual_queries=1,
+        classical_expected_queries=pytest.approx(1.0),
+        quantum_expected_classical_queries=pytest.approx(1.0),
+        quantum_expected_quantum_queries=pytest.approx(0),
+    )
+
+
+def test_mixed_solve(rng) -> None:
+    """Test variant of Schöning's algorithm and quantum statistic generation
+
+    Args:
+        rng (np.rng): Source of randomness provided by test fixtures
+    """
+    # solve a simple SAT instance
+    inst = SatInstance(
+        k=3,
+        clauses=np.array([[1, 1, -1], [1, -1, 1], [-1, 1, 1], [1, -1, -1]], dtype=int),
+    )
+    stats = QueryStats()
+    x = schoening_mixed_solve(inst, rng=rng, error=10**-5, stats=stats)
     assert x is not None
 
     # check that found a solution
