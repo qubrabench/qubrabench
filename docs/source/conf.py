50c4497a0e49595bd99aaf05e05a3eef33e14782
Anurudh Peduri <7265746+anurudhp@users.noreply.github.com>
2023-07-25 14:07:12 +0200

    [docs] add readme.md, develop.md, and missing links (#87)
    * [docs] add readme.md and develop.md, and github repo+fork links
    
    * add myst-parser to dev deps
    
    * add myst-parser
    
    * add missing sources
    
    * Links use absolute path
    
    * [docs] Add logo and banner
    
    Move banner to static content to avoid AWS 403 error
    
    fix #87
    
    ---------
    
    Co-authored-by: Luca <witt-luca@web.de>
    Co-authored-by: Tim Bittner <timbittner21@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ release = "0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.napoleon", "myst_parser"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -33,4 +33,27 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
-html_static_path = ["_static"]
+html_static_path = ["../img"]
+
+# -- HTML navigation bar -----------------------------------------------------
+html_theme_options = {
+    "github_user": "qubrabench",
+    "github_repo": "qubrabench",
+    "description": "A framework to benchmark the advantage of quantum algorithms.",
+    "github_banner": "forkme_right_darkblue_121621.png",
+    "logo": "logo.png",
+    "logo_name": True,
+    "github_type": "star",
+    "extra_nav_links": {
+        "GitHub": "https://github.com/qubrabench/qubrabench/",
+        "Issue Tracker": "https://github.com/qubrabench/qubrabench/issues",
+    },
+}
+html_sidebars = {
+    "**": [
+        "about.html",
+        "localtoc.html",
+        "navigation.html",
+        "searchbox.html",
+    ]
+}
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,18 +3,26 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to qubrabench's documentation!
-======================================
+Welcome!
+========
+
+qubrabench is a Python library that aims to enable domain experts to estimate quantum speedups provided by future quantum computers for realistic use cases and data sets, in advance of large-scale quantum computers being available.
 
 .. toctree::
    :maxdepth: 2
 
    modules
 
+.. toctree::
+   :maxdepth: 1
+
+   readme
+   develop
+   license
 
 
 Indices and tables
-==================
+------------------
 
 * :ref:`genindex`
 * :ref:`modindex`
