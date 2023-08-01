4c2cf6f2aa7b2c24b7efb2d66d2fb3c2bb1c3e5f
Anurudh Peduri <anurudhp97@gmail.com>
2023-08-01 23:25:09 +0200

    fix some comments

--- qubrabench/algorithms/search.py
+++ qubrabench/algorithms/search.py
@@ -15,15 +15,15 @@ E = TypeVar("E")
 
 class SearchDomain(ABC, Generic[E]):
     @abstractmethod
-    def N(self) -> float:
+    def get_size(self) -> int:
         pass
 
     @abstractmethod
-    def T(self, key) -> float:
+    def get_number_of_solutions(self, key) -> float:
         pass
 
     @abstractmethod
-    def get_sample(self, rng: np.random.Generator) -> E:
+    def get_random_sample(self, rng: np.random.Generator) -> E:
         pass
 
 
@@ -33,13 +33,13 @@ class ListDomain(SearchDomain[E], Generic[E]):
         rng.shuffle(self.it)
         self.sample_it = iter(self.it)
 
-    def N(self):
+    def get_size(self):
         return len(self.it)
 
-    def T(self, key):
+    def get_number_of_solutions(self, key):
         return len([1 for x in self.it if key(x)])
 
-    def get_sample(self, rng: np.random.Generator) -> E:
+    def get_random_sample(self, rng: np.random.Generator) -> E:
         try:
             x = next(self.sample_it)
         except StopIteration:
@@ -79,7 +79,7 @@ def search(
     if isinstance(seq, Iterable):
         seq = ListDomain(seq, rng)
 
-    N = seq.N()
+    N = seq.get_size()
 
     # collect stats
     if stats:
@@ -88,7 +88,7 @@ def search(
                 "search() parameter 'error' not provided, cannot compute quantum query statistics"
             )
 
-        T = seq.T(key)
+        T = seq.get_number_of_solutions(key)
 
         stats.classical_expected_queries += (N + 1) / (T + 1)
         stats.quantum_expected_classical_queries += (
@@ -100,7 +100,7 @@ def search(
 
     # run the classical sampling-without-replacement algorithms
     while True:
-        x = seq.get_sample(rng)
+        x = seq.get_random_sample(rng)
         if x is None:
             return None
 
@@ -110,9 +110,7 @@ def search(
             return x
 
 
-def cade_et_al_expected_quantum_queries(
-    N: float, T: float, eps: float, K: int
-) -> float:
+def cade_et_al_expected_quantum_queries(N: int, T: float, eps: float, K: int) -> float:
     """Upper bound on the number of *quantum* queries made by Cade et al's quantum search algorithm.
 
     Args:
@@ -132,7 +130,7 @@ def cade_et_al_expected_quantum_queries(
     return (1 - (T / N)) ** K * F * (1 + (1 / (1 - (F / (9.2 * np.sqrt(N))))))  # type: ignore
 
 
-def cade_et_al_expected_classical_queries(N: float, T: float, K: int) -> float:
+def cade_et_al_expected_classical_queries(N: int, T: float, K: int) -> float:
     """Upper bound on the number of *classical* queries made by Cade et al's quantum search algorithm.
 
     Args:
@@ -150,7 +148,7 @@ def cade_et_al_expected_classical_queries(N: float, T: float, K: int) -> float:
     return (N / T) * (1 - (1 - (T / N)) ** K)
 
 
-def cade_et_al_F(N: float, T: float) -> float:
+def cade_et_al_F(N: int, T: float) -> float:
     """
     Return quantity F defined in Eq. (3) of Cade et al.
     """
