f5aa9802c9c2171eb76b30e5ddc48ea7de7f369e
Anurudh Peduri <7265746+anurudhp@users.noreply.github.com>
2024-01-12 17:17:49 +0100

    Quantum Linear solver from https://arxiv.org/abs/2305.11352 (#105)
    * init: lin solver from https://arxiv.org/abs/2305.11352
    
    * nested costs for implementing block-enc
    
    * add first test
    
    * more tests
    
    * paper assumes inputs are perfect block-encodings
    
    * query cost to block-encode dense matrix.

---
+++ qubrabench/algorithms/linalg.py
@@ -0,0 +1,89 @@
+from typing import Optional, Hashable
+import numpy as np
+
+from ..benchmark import QueryStats
+from ..datastructures.blockencoding import BlockEncoding
+
+__all__ = ["solve"]
+
+
+def solve(
+    A: BlockEncoding,
+    b: BlockEncoding,
+    *,
+    error: Optional[float] = None,
+    condition_number_A: Optional[float] = None,
+) -> BlockEncoding:
+    """Quantum Linear Solver as described in https://arxiv.org/abs/2305.11352.
+
+    Given block-encodings for $A$ and $b$, find an approximation of $y$ satisfying $Ay = b$.
+
+    Args:
+        A: block-encoded input matrix
+        b: block-encoded input vector
+        error: approximation factor of the solution vector
+        condition_number_A: An upper-bound on the condition number of A. Optional, will be calculated if not provided.
+
+    Returns:
+        Block-encoded solution vector.
+    """
+
+    costs: dict[Hashable, QueryStats] = {}
+
+    if error is not None:
+        if condition_number_A is None:
+            condition_number_A = np.linalg.cond(A.matrix)
+
+        if not np.isclose(A.error, 0):
+            raise ValueError(
+                f"solve expects a zero-error block-encoding of A, but input has an error of {A.error}"
+            )
+        if not np.isclose(b.error, 0):
+            raise ValueError(
+                f"solve expects a zero-error block-encoding of b, but input has an error of {b.error}"
+            )
+
+        condition_number_A = max(condition_number_A, np.sqrt(12))
+        error = min(error, 0.24)
+
+        alpha, k, eps = A.alpha, condition_number_A, error
+
+        q_star_term_1 = (
+            (1741 * alpha * eps / 500)
+            * np.sqrt(k**2 + 1)
+            * (
+                (133 / 125 + 4 / (25 * np.power(k, 1.0 / 3)))
+                * np.pi
+                * np.log(2 * k + 3)
+                + 1
+            )
+        )
+        q_star_term_2 = (
+            (351 / 50)
+            * np.log(2 * k + 3) ** 2
+            * (np.log(451 * np.log(2 * k + 3) ** 2 / eps) + 1)
+        )
+        q_star_term_3 = alpha * k * np.log(32 / eps)
+
+        q_star = q_star_term_1 + q_star_term_2 + q_star_term_3
+        q = q_star / (0.39 - 0.201 * eps)
+
+        for obj, stats in A.costs.items():
+            if obj not in costs:
+                costs[obj] = QueryStats()
+            costs[obj] += QueryStats(
+                quantum_expected_quantum_queries=(
+                    q * stats.quantum_expected_quantum_queries
+                )
+            )
+        for obj, stats in b.costs.items():
+            if obj not in costs:
+                costs[obj] = QueryStats()
+            costs[obj] += QueryStats(
+                quantum_expected_quantum_queries=(
+                    2 * q * stats.quantum_expected_quantum_queries
+                )
+            )
+
+    y = np.linalg.solve(A.matrix, b.matrix)
+    return BlockEncoding(y, alpha=np.linalg.norm(y), error=error, costs=costs)
---
+++ tests/algorithms/test_linalg.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pytest
+
+from qubrabench.benchmark import track_queries
+from qubrabench.datastructures.blockencoding import BlockEncoding
+from qubrabench.algorithms.linalg import solve
+
+
+def random_instance(rng, N: int) -> tuple[BlockEncoding, BlockEncoding]:
+    A = rng.random(size=(N, N))
+    b = rng.random(size=N)
+
+    enc_A = BlockEncoding(A, alpha=np.linalg.norm(A), error=0)
+    enc_b = BlockEncoding(b, alpha=np.linalg.norm(b), error=0)
+
+    return enc_A, enc_b
+
+
+@pytest.mark.parametrize("N", [5, 10, 15, 20, 25, 30])
+def test_solve(rng, N: int):
+    enc_A, enc_b = random_instance(rng, N)
+    enc_y = solve(enc_A, enc_b)
+    np.testing.assert_allclose(enc_A.matrix @ enc_y.matrix, enc_b.matrix)
+
+
+def test_solve_stats(rng):
+    N = 10
+    enc_A, enc_b = random_instance(rng, N)
+    enc_y = solve(enc_A, enc_b, error=1e-5)
+
+    with track_queries() as tracker:
+        enc_y.get()
+        queries_y = tracker.get_stats(enc_y).quantum_expected_quantum_queries
+        queries_A = tracker.get_stats(enc_A).quantum_expected_quantum_queries
+        queries_b = tracker.get_stats(enc_b).quantum_expected_quantum_queries
+
+    assert queries_y == 1
+    assert queries_A == pytest.approx(840211.9909199567)
+    assert queries_b == pytest.approx(2 * queries_A)
