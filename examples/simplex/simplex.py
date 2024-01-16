89ac8229248a7ddd701d125f5272397dd46fa5f5
Anurudh Peduri <anurudhp97@gmail.com>
2024-01-16 14:31:38 +0100

    add more info

--- examples/simplex/simplex.py
+++ examples/simplex/simplex.py
@@ -41,7 +41,7 @@ def Simplex(A: Matrix, b: Vector, c: Vector):
 def SimplexIter(
     A: Matrix, B: Basis, c: Vector, epsilon: float, delta: float, t: float
 ) -> Union[ResultFlag, tuple[int, int]]:
-    """Algorithm 1 of the paper.
+    """Algorithm 1 [C->C]: Run one iteration of the simplex method
 
     Args:
         A: matrix
@@ -77,7 +77,7 @@ def SimplexIter(
 
 
 def Interfere(U: BlockEncoding, V: BlockEncoding) -> BlockEncoding:
-    r"""Algorithm 2: Interference Routine
+    r"""Algorithm 2 [Q->Q]: Interference Routine
 
     Given unitaries $U, V$ such that $U\ket{0} = \sum_j \alpha_j \ket j$ and $U\ket{0} = \sum_j \beta_j \ket j$,
     returns a unitary that prepares
@@ -94,46 +94,68 @@ def Interfere(U: BlockEncoding, V: BlockEncoding) -> BlockEncoding:
     raise NotImplementedError
 
 
-def SignEstNFN(U, k, epsilon):
-    """Algorithm 3"""
+def SignEstNFN(U, k, epsilon) -> bool:
+    """Algorithm 3 [Q->C]: Sign estimation routine"""
     raise NotImplementedError
 
 
 def RedCost(A_B, A_k, c, epsilon):
-    """Algorithm 4"""
+    """Algorithm 4 [Q->Q]: Determining the reduced cost of a column"""
     raise NotImplementedError
 
 
-def CanEnter(A_B, A_k, c, epsilon):
-    """Algorithm 5"""
+def CanEnter(A_B: Matrix, A_k: Vector, c: Vector, epsilon: float) -> bool:
+    r"""Algorithm 5 [C->C]: Determine is a column is eligible to enter the basis
+
+    Returns:
+        1 if the nonbasic column $A_k$ has reduced cost $< \epsilon$;
+        0 otherwise
+    """
     raise NotImplementedError
 
 
-def FindColumn(A, B, c, epsilon):
-    """Algorithm 6"""
+def FindColumn(A: Matrix, B: Basis, c: Vector, epsilon: float) -> int:
+    r"""Algorithm 6 [C->C]: Determine the next column to enter the basis
+
+    Args:
+        A: matrix s.t. $\norm{A_B} \le 1$
+        B: basis
+        c: cost vector s.t. $\norm{c_B} = 1$
+        epsilon: precision
+
+    Returns:
+        index of column $k$ with $\bar{c}_k < \epsilon \norm{(A_B^{-1} A_k, c_k)} if one exists, with bounded probability.
+    """
     raise NotImplementedError
 
 
-def IsOptimal(A, B, c, epsilon):
-    """Algorithm 6b"""
+def IsOptimal(A: Matrix, B: Basis, c: Vector, epsilon: float) -> bool:
+    r"""Algorithm 6b [C->C]
+
+    Args:
+        A: matrix s.t. $\norm{A_B} \le 1$
+        B: basis
+        c: cost vector s.t. $\norm{c_B} = 1$
+        epsilon: precision
+    """
     raise NotImplementedError
 
 
-def SteepestEdgeCompare(A_B, A_j, A_k, c, epsilon):
-    """Algorithm 7"""
+def SteepestEdgeCompare(A_B, A_j, A_k, c, epsilon) -> bool:
+    """Algorithm 7 [C->C]: Compare the steepest edge price of two columns"""
     raise NotImplementedError
 
 
-def IsUnbounded(A_B, A_k, delta):
-    """Algorithm 8"""
+def IsUnbounded(A_B, A_k, delta) -> bool:
+    """Algorithm 8 [C->C]: Determine if the problem is unbounded from below"""
     raise NotImplementedError
 
 
-def FindRow(A_B, A_k, b, delta, t):
-    """Algorithm 9"""
+def FindRow(A_B, A_k, b, delta, t) -> int:
+    """Algorithm 9 [C->C]: Determine the basic variable (row) leaving the basis"""
     raise NotImplementedError
 
 
-def IsFeasible(A_B, b, delta):
-    """Algorithm 10"""
+def IsFeasible(A_B, b, delta) -> bool:
+    """Algorithm 10 [C->C]: Determine if a basic solution is feasible"""
     raise NotImplementedError
