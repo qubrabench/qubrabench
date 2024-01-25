9c34b3c06c99b188f7e8c4c9c115cda26a0abebe
Anurudh Peduri <7265746+anurudhp@users.noreply.github.com>
2024-01-25 22:35:08 +0100

    fix fcs graph generator (#118)
    * fix fcs graph generator
    
    * fix stats (still not sanity checked)

--- examples/communities/test_graph_instances.py
+++ examples/communities/test_graph_instances.py
@@ -12,26 +12,4 @@ def test_fcs_generation(rng):
     G = random_fcs_graph(10, community_size=3, mu=0.3, average_degree=5, rng=rng)
 
     assert len(G.nodes) == 10
-    assert list(G.edges) == [
-        (1, 7),
-        (1, 2),
-        (1, 9),
-        (2, 6),
-        (2, 3),
-        (3, 4),
-        (3, 6),
-        (3, 5),
-        (4, 5),
-        (4, 6),
-        (5, 6),
-        (5, 7),
-        (5, 9),
-        (6, 10),
-        (6, 8),
-        (6, 9),
-        (7, 9),
-        (7, 8),
-        (8, 10),
-        (8, 9),
-        (9, 10),
-    ]
+    assert len(G.edges) == 25
