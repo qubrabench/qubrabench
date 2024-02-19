443e4db21715653ed89ed67550f508e677d30d8d
Anurudh Peduri <anurudhp97@gmail.com>
2024-02-19 22:42:13 +0100

    check stats

--- examples/test_nesting.py
+++ examples/test_nesting.py
@@ -1,6 +1,8 @@
 import pytest
 import numpy as np
 from nesting import example
+from qubrabench.benchmark import track_queries
+from pprint import pprint
 
 
 @pytest.mark.parametrize("N", [4, 10, 20])
@@ -14,3 +16,16 @@ def test_example_planted(rng, N: int):
         expected = np.any(np.abs(x) >= 0.5)
         actual = example(A, b)
         assert expected == actual
+
+
+def test_example_planted_stats(rng):
+    N = 20
+    A = rng.random((N, N))
+    x = rng.random(N)
+    x /= np.linalg.norm(x)
+    b = A @ x
+
+    with track_queries() as tracker:
+        result = example(A, b)
+        print()
+        pprint(tracker.stats)
