b68ac14854c8656224518d5fd1b6d4f46bbfe677
Anurudh Peduri <anurudhp97@gmail.com>
2024-02-20 15:27:31 +0100

    rename

--- tests/datastructures/test_qndarray.py
+++ tests/datastructures/test_qndarray.py
@@ -4,7 +4,7 @@ from qubrabench.benchmark import QueryStats, track_queries
 from qubrabench.datastructures.qndarray import Qndarray
 
 
-def test_qmatrix_iterate(rng):
+def test_qndarray_iterate(rng):
     for _ in range(5):
         N, M = rng.integers(10, 50, size=2)
         mat = rng.random(size=(N, M))
@@ -29,7 +29,7 @@ def test_qmatrix_iterate(rng):
             )
 
 
-def test_matrix_idempotent():
+def test_qndarray_constructor_idempotent():
     a = Qndarray(np.eye(3))
     b = Qndarray(a)
     assert a == b
