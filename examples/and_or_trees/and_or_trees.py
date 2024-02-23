de8eadc4b399cbe19b90aee55a73c6e59c3e1a03
Anurudh Peduri <anurudhp97@gmail.com>
2024-02-23 21:15:10 +0100

    add test, account for failprob

--- examples/and_or_trees/and_or_trees.py
+++ examples/and_or_trees/and_or_trees.py
@@ -7,17 +7,28 @@ from qubrabench.algorithms.search import search
 
 class AndOrTree(ABC):
     @abstractmethod
-    def evaluate(self, xs: Sequence[bool]) -> bool:
-        pass
+    def evaluate(self, xs: Sequence[bool], *, max_failure_probability: float) -> bool:
+        """evaluate the formula on an instance"""
 
 
 @dataclass
 class AndNode(AndOrTree):
     subtrees: list[AndOrTree]
 
-    def evaluate(self, xs: Sequence[bool]) -> bool:
+    def evaluate(self, xs: Sequence[bool], *, max_failure_probability: float) -> bool:
         return (
-            search(self.subtrees, key=lambda tree: tree.evaluate(xs) is False) is None
+            search(
+                self.subtrees,
+                key=lambda tree: tree.evaluate(
+                    xs,
+                    max_failure_probability=(
+                        max_failure_probability / (2 * len(self.subtrees))
+                    ),
+                )
+                is False,
+                max_failure_probability=max_failure_probability / 2,
+            )
+            is None
         )
 
 
@@ -25,13 +36,25 @@ class AndNode(AndOrTree):
 class OrNode(AndOrTree):
     subtrees: list[AndOrTree]
 
-    def evaluate(self, xs: Sequence[bool]) -> bool:
-        return search(self.subtrees, key=lambda tree: tree.evaluate(xs)) is not None
+    def evaluate(self, xs: Sequence[bool], *, max_failure_probability: float) -> bool:
+        return (
+            search(
+                self.subtrees,
+                key=lambda tree: tree.evaluate(
+                    xs,
+                    max_failure_probability=(
+                        max_failure_probability / (2 * len(self.subtrees))
+                    ),
+                ),
+                max_failure_probability=max_failure_probability / 2,
+            )
+            is not None
+        )
 
 
 @dataclass
 class LeafNode(AndOrTree):
     index: int
 
-    def evaluate(self, xs: Sequence[bool]) -> bool:
+    def evaluate(self, xs: Sequence[bool], *, max_failure_probability: float) -> bool:
         return xs[self.index]
